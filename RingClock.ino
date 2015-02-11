94c70b7b528929ca79cbb90f0117929b02e305b7
Ian Curtis <ian.curtisnz@gmail.com>
2015-02-12 09:57:19 +1300

    RTC Clock Setup

--- RingClock.ino
+++ RingClock.ino
@@ -1,9 +1,24 @@
+#include <Wire.h>
+#include "RTClib.h"
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(57600);
+  Wire.begin();
+  RTC.begin();
+  //Check if RTC running, if not Set time.
+  if (! RTC.isrunning()) {
+  Serial.println("RTC is NOT running!");
+  //Sets RTC to Time Sketch Uploaded
+  RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int hour, minute;
+  DateTime currentTime = RTC.now();
+  hour = currentTime.hour();
+  minute = currentTime.minute();
 
 }
